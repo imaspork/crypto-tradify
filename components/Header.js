760776b7ea9dc7aedb531402862bb5aee07f5535
Tyler Strong <tyler.strong1@gmail.com>
2022-04-18 12:17:49 -0700

    Simple skeleton changes, styles, components etc

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section
+      id='header'
+      className='d-flex flex-row justify-content-around align-items-center'
+    >
+      <h1>Crypto Tradify</h1>
+    </section>
+  );
+};
+
+export default Header;
---
+++ components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <section id='home-page' className='d-flex flex-row justify-content-center'>
+      test
+    </section>
+  );
+};
+
+export default Homepage;
