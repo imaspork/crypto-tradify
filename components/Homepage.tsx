bc73fa8d0ec9f63ac22cb5007a6052b299028120
Tyler Strong <tyler.strong1@gmail.com>
2022-06-12 10:46:21 -0700

    conditional button changes to home page

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -5,20 +5,9 @@ import { useSession } from "next-auth/client";
 const Homepage = () => {
   const [session, loadingSession] = useSession();
 
-  return (
-    <section
-      id='home-page'
-      className='d-flex flex-row justify-content-center align-items-center'
-    >
-      <div className='d-flex flex-column align-items-center'>
-        <h1 className='welcome-h1'>Welcome.</h1>
-        <Link href='/Account'>
-          <a>
-            <button className='button-primary mt-3 homepage-button'>
-              {session ? "Account Page" : "Sign Up"}
-            </button>
-          </a>
-        </Link>
+  const RenderButtonsOnSession = () => { 
+    if (session) {
+      return <React.Fragment>
         <Link href='/Crypto'>
           <a>
             <button className='button-primary mt-3 homepage-button'>
@@ -26,6 +15,32 @@ const Homepage = () => {
             </button>
           </a>
         </Link>
+      </React.Fragment>
+    } else {
+      return (
+        <React.Fragment>
+        <Link href='/Account'>
+          <a>
+            <button className='button-primary mt-3 homepage-button'>
+              {session ? "Account Page" : "Get started"}
+            </button>
+          </a>
+        </Link>
+      </React.Fragment>
+      )
+    }
+  }
+
+  return (
+    <section
+      id='home-page'
+      className='d-flex flex-row justify-content-center align-items-center'
+    >
+      <div id="tagline">
+        <h1><b>Buy, Sell, and Hold the top 100 Cryptocurrencies (simulated)</b> </h1>
+      </div>
+      <div className='d-flex flex-column align-items-center'>
+        <RenderButtonsOnSession />
       </div>
     </section>
   );
